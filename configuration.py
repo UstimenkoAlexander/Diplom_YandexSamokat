f3ed4d9e940807db771e7de7ee0a4845a6d9e41b
UstimenkoAlexander <149778124+UstimenkoAlexander@users.noreply.github.com>
2023-11-09 12:01:33 +0500

    Creat

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = " https://a667492f-7cd8-407b-88d5-e52183c7d50d.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://4006d0f5-a11e-4f0c-bf45-1cca0519533f.serverhub.praktikum-services.ru"
 CREATE_NEW_ORDER = "/api/v1/orders"
 RECEIVING_ORDER_BY_ID = "/api/v1/orders/track"
